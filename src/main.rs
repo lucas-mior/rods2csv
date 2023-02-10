6a7577520e2826bd11ee5f2e56663d916b552229
lucas-mior <lucas.mior@tutamail.com>
2023-02-10 15:09:21 -0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@ use std::io;
 use std::process::exit;
 use regex::Regex;
 
+fn worka(caps: &regex::Captures) {
+    let mut string = String::new();
+    let x = caps.get(1).map_or("", |m| m.as_str());
+    let mut a = x.parse::<usize>().unwrap();
+    if a > 30 {
+        a = 30;
+    }
+    for _ in 0..a {
+        string.push_str("<table-cell/>");
+    }
+    println!("{}", string);
+}
+
+
 fn main() {
     println!("============== rods2csv ==============");
     let mut row: u32 = 0;
 
-    let mut string = String::new();
-    // let mut content;
-    // let mut fina;
     let table_rep = Regex::new("table-cell.*number-columns-repeated").unwrap();
     let table_span = Regex::new("table-cell.*number-columns-spanned").unwrap();
     let any_table = Regex::new("table-cell.*number-columns-(repeated|spanned)").unwrap();
@@ -16,32 +27,82 @@ fn main() {
     let trep_a = Regex::new(".*repeated=\"([[:digit:]]+)\"[^>]*/>").unwrap();
     let trep_b = Regex::new(".*repeated=\"([[:digit:]]+)\"[^>/]*>").unwrap();
 
-    let table_row = Regex::new("table-row").unwrap();
+    let tspan_a = Regex::new(".*columns-spanned=\"([[:digit:]]+)\"[^>]*/>").unwrap();
+    let tspan_b = Regex::new(".*columns-spanned=\"([[:digit:]]+)\"[^>/]*>").unwrap();
 
-    for line in io::stdin().lines() {
+    let table_row = Regex::new("table-row").unwrap();
+    let table_cell = Regex::new("</table-cell>").unwrap();
+    let cont = Regex::new("<[^>]*>").unwrap();
+
+    let mut lines = io::stdin().lines();
+    while let Some(line) = lines.next() {
         let line = line.unwrap();
         if table_rep.is_match(&line) {
-            println!("repeated: {}", line);
-            let caps = match trep_a.captures(&line) {
-                Some(caps) => caps,
+            match trep_a.captures(&line) {
+                Some(caps) => worka(&caps),
                 _ => match trep_b.captures(&line) {
-                    Some(caps) => caps,
+                    Some(caps) => {
+                        let mut string = String::new();
+                        let mut content = String::new();
+                        let mut fina = String::new();
+                        let x = caps.get(1).map_or("", |m| m.as_str());
+                        let mut b = x.parse::<usize>().unwrap();
+                        if b > 50 {
+                            b = 50;
+                        }
+                        while ! table_cell.is_match(&line) {
+                            if let Some(line) = lines.next() {
+                                let line = match line {
+                                    Ok(line) => line,
+                                    Err(_) => break,
+                                };
+                                content.push_str(&line);
+                            } else {
+                                break;
+                            }
+                        }
+                        let content = cont.replace(&content, "");
+                        for _ in 0..b {
+                            fina = format!("{}<table-cell>{}</table-cell>", fina, content);
+                        }
+                        println!("{}", fina);
+                    }
                     _ => {println!("caps failed"); exit(1);},
                 },
             };
-            println!("caps: {}", line);
-            let x = caps.get(1).map_or("", |m| m.as_str());
-            println!("x: {}", x);
-            let mut a = x.parse::<usize>().unwrap();
-            if a > 30 {
-                a = 30;
-            }
-            for _ in 0..a {
-                string.push_str("<table-cell/>");
-            }
-            println!("String: {}", string);
         } else if table_span.is_match(&line) {
-            println!("spanned: {}", line);
+            match tspan_a.captures(&line) {
+                Some(caps) => worka(&caps),
+                _ => match tspan_b.captures(&line) {
+                    Some(caps) => {
+                        let mut string = String::new();
+                        let mut content = String::new();
+                        let mut fina = String::new();
+                        let x = caps.get(1).map_or("", |m| m.as_str());
+                        let mut b = x.parse::<usize>().unwrap();
+                        if b > 50 {
+                            b = 50;
+                        }
+                        while ! table_cell.is_match(&line) {
+                            if let Some(line) = lines.next() {
+                                let line = match line {
+                                    Ok(line) => line,
+                                    Err(_) => break,
+                                };
+                                content.push_str(&line);
+                            } else {
+                                break;
+                            }
+                        }
+                        let content = cont.replace(&content, "");
+                        for _ in 0..b {
+                            fina = format!("{}<table-cell>{}</table-cell>", fina, content);
+                        }
+                        println!("{}", fina);
+                    }
+                    _ => {println!("caps failed"); exit(1);},
+                },
+            };
         } else if ! any_table.is_match(&line) {
             println!("not any table: {}", line);
             if table_row.is_match(&line) {
